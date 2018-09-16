397635d9094cc9231aeb3b10af681448e97cd660
Paul Kepley <pakepley@gmail.com>
2018-09-16 13:47:33 -0500

    Black and white image segmenter based on quick union.

---
+++ ImageSegmenter.py
@@ -0,0 +1,33 @@
+import QuickUnion
+
+class ImageSegmenter:
+    def __init__(self, img):        
+        self.img = img
+        #self.flat_img = self.img.flatten()
+        self.nx = self.img.shape[0]
+        self.ny = self.img.shape[1]
+        self.np = self.nx * self.ny
+        self.qu = QuickUnion.QuickUnion(self.np)
+
+    def __ij_to_k__(self, i, j):
+        return i * self.ny + j
+        
+    def __k_to_ij__(self, k):
+        i = k / self.ny
+        j = k % self.ny
+    
+    def constructComps(self):
+        for i in range(1, self.nx-1):
+            print(i, self.nx, self.ny)
+            for j in range(1, self.ny-1):
+                if self.img[i,j] == 1:
+                    k = self.__ij_to_k__(i,j)
+                    nbrs  =  self.img[i-1:i+2, j-1:j+2].flatten()
+                    for l in [0,1,2,3,5,6,7,8]:
+                        if nbrs[l] == 1:
+                            ii = (i - 1) + (l / 3)
+                            jj = (j - 1) + (l % 3)
+                            kk = self.__ij_to_k__(ii,jj)
+                            if not self.qu.find(k, kk):
+                                self.qu.unite(k, kk)
+        self.qu.updateAll()
---
+++ QuickUnion.py
@@ -0,0 +1,20 @@
+class QuickUnion:
+    def __init__(self, N):
+        self.id = range(N)
+    
+    def root(self, i):
+        while (i != self.id[i]):
+            i = self.id[i]
+        return i
+
+    def unite(self, p, q):
+        i = self.root(p)
+        j = self.root(q)
+        self.id[i] = j
+
+    def find(self, p, q):
+        return self.root(p) == self.root(q)
+
+    def updateAll(self):
+        for i in range(len(self.id)):
+            self.id[i] = self.root(i)
